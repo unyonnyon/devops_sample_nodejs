4e199359fba73e258bb2645fdd85642443047d68
キングスライム <kings@sunaoshiyuukanoMacBook-Pro.local>
2020-05-11 22:12:54 +0900

    Add DSL

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job("NodeJS Job") {
+    scm {
+        git("https://github.com/unyonnyon/devops_sample_nodejs.git"){ node ->
+            node / gitConfigName("DSL User")
+            node / gitConfigEmail("jenkins-dsl@example.com")
+        }
+    }
+    triggers {
+        scm("H/5 * * * *")
+    }
+    wrappers {
+        nodejs("nodejs_v13")
+    }
+    steps {
+        shell("npm install")
+        shell("npm test")
+    }
+}
